728c1ac64f98b754076b0004e15a7476d36629d4
YogaLiao <liaoyujia0702@gmail.com>
2022-07-13 16:14:24 -0500

    model alter

--- catty/models.py
+++ catty/models.py
@@ -1,7 +1,7 @@
-from pickle import TRUE
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 import uuid
+
 
 # Create your models here.
 class UserInfo(models.Model):
@@ -28,10 +28,10 @@ class Service(models.Model):
     username = models.ForeignKey(UserInfo, on_delete=models.CASCADE, related_name= 'services')
     displayName = models.CharField(max_length=100, default=username)
     headline = models.CharField(max_length=100, default='')
-    service = models.CharField(max_length=10, default='onboarding')
+    service = models.CharField(max_length=100, default='onboarding')
     rate = models.IntegerField()
-    note = models.CharField(max_length=100, default='')
-    disable = ArrayField(ArrayField(models.CharField(max_length=20, blank = TRUE)))
+    note = models.CharField(max_length=100, default='', blank = True)
+    disable = ArrayField(models.CharField(max_length=20, blank = True))
 
     def __str__(self):
         return self.service
